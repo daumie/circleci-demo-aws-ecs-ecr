a7395a2e4e62b8aa389ab832fe88bea98a17ec87
Dominic Motuka <dominic.motuka@gmail.com>
2019-08-04 02:59:13 +0300

    Add ignore file
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform_setup/terraform.tf
+++ terraform_setup/terraform.tf
@@ -2,7 +2,6 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
-  version = "~> 1.35"
 }
 
 locals {
@@ -27,7 +26,7 @@ resource "aws_cloudformation_stack" "vpc" {
   name = "${local.aws_vpc_stack_name}"
   template_body = "${file("cloudformation-templates/public-vpc.yml")}"
   capabilities = ["CAPABILITY_NAMED_IAM"]
-  parameters {
+  parameters = {
     ClusterName = "${local.aws_ecs_cluster_name}"
     ExecutionRoleName = "${local.aws_ecs_execution_role_name}"
   }
@@ -39,7 +38,7 @@ resource "aws_cloudformation_stack" "ecs_service" {
   template_body = "${file("cloudformation-templates/public-service.yml")}"
   depends_on = ["aws_cloudformation_stack.vpc", "aws_ecr_repository.demo-app-repository"]
 
-  parameters {
+  parameters = {
     ContainerMemory = 1024
     ContainerPort = 80
     StackName = "${local.aws_vpc_stack_name}"
